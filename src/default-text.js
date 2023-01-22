4de4445045e554c46f96bee2001fa550ec4dc3f0
Michael Diakonov <diakonov.m@gmail.com>
2023-01-22 11:23:31 -0500

    default text

---
+++ src/default-text.js
@@ -0,0 +1,17 @@
+const text = `[Markdown Guide Basic Syntax](https://www.markdownguide.org/basic-syntax)
+
+### Who was Claudio Bravo?
+
+[Claudio Bravo](<https://en.wikipedia.org/wiki/Claudio_Bravo_(painter)>) was a Chilean hyperrealist painter well-known for his paintings of still lifes, portraits, packages and **paper**.
+
+#### Public Collections
+
+| Museum | Location |
+| -------------------------------------------- | --------------------- |
+| Museum of Fine Arts | Boston, Massachusetts |
+| Museum Ludwig | Cologne, Germany |
+| Art Museum, Florida International University | Miami, Florida |
+| The Metropolitan Museum of Art | New York, New York |
+`
+
+export default text
